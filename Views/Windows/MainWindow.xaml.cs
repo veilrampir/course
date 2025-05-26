7bbee90c55505b0ea062b80fa3bc3199cfd619bc
veilrampir <veilrampir@mail.ru>
2025-05-26 22:17:38 +0300

    конец 1-го урока

--- Views/Windows/MainWindow.xaml.cs
+++ Views/Windows/MainWindow.xaml.cs
@@ -1,15 +1,52 @@
 ﻿
 using System.Windows;
-
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Axes;
+using OxyPlot.Wpf;
+using System;
+using System.Xaml.Schema;
+using course.ViewModels;
+using System.Windows.Data;
+using course.Models;
+using course.ViewModels.Base;
 
 namespace course
 {
-  
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            var model = new PlotModel();
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom });
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Left });
+
+            var series = new LineSeries()
+            {
+                Color = OxyColors.Red,
+                StrokeThickness = 2,
+                ItemsSource = ((MainWindowViewModel)DataContext).TestDataPoints,
+
+                DataFieldX = "XValue",
+                DataFieldY = "YValue"
+
+            };
+
+            model.Series.Add(series);
+
+            ((PlotView)FindName("plotView")).Model = model;
+
+
+
+
+        }
+
+        private void ContextMenu_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
